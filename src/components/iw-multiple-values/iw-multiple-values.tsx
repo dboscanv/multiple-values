e9ce5b38d4d93c94246fccb487e4f67e97887e44
Diego Boscan <diego@iugo.com.uy>
2018-07-26 00:34:41 -0300

    [WIP] Lack other types of inputs than 'text'

--- src/components/iw-multiple-values/iw-multiple-values.tsx
+++ src/components/iw-multiple-values/iw-multiple-values.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, State } from '@stencil/core';
+import { Component, Element, State, Prop } from '@stencil/core';
 import { Item } from './Item';
 
 // La metadata le agrega propiades/metodos a la clase
@@ -15,9 +15,9 @@ import { Item } from './Item';
 export class IwMultipleValues {
 
   @Element() element: HTMLElement;
+  @Prop() label: string;
   @State() data: any[];
-  @State() test: any[] = [{a:1,b:2},{a:2,b:4},{a:3,b:6},{a:4,b:8},{a:5,b:10}];
-
+  
   // Array of inputs
   model: Item[] = [];
   modelObj: object;
@@ -36,58 +36,50 @@ export class IwMultipleValues {
     }
   }
 
+  componentDidUpdate() {
+    // When re-render the component, update the value of the element
+    this.element.setAttribute('value', JSON.stringify(this.data));
+  }
+
   makeObject() {
     let obj = {};
     for (let item of this.model) {
       obj[item.name] = item.value;
     }
-    this.modelObj = {...obj}; 
+    this.modelObj = { ...obj };
     return obj;
   }
 
   addItem() {
-    this.data = [...this.data, {...this.modelObj}];
+    this.data = [...this.data, { ...this.modelObj }];
   }
 
-  changeValue(event, index, property) {
+  removeItem(index: number) {
+    if (this.data.length == 1) {
+      this.data = [this.makeObject()];
+    } else {
+      this.data = this.data.filter((_, idx) => idx !== index);
+    }
+  }
+
+  changeValue(event, index: number, property: string) {
     let value = event.target.value;
     // this.data[index][property] = value; // Aca no hay reactividad
     this.data = this.data.map((i, idx) => {
-      if(idx == index) {
+      if (idx == index) {
         i[property] = value;
       }
       return i;
-    });    
-  }
-
-  changeFirstOnly() {
-    // // TODO: Ver como hacer para sea reactivo?
-    this.data = this.data.map((i,idx) => {
-      if (idx == 0) {
-        i.first_name = "DIEGO";
-      }
-      return i;
     });
-    // this.test = this.test.map((i, idx) => {
-    //   if(idx == 0) {
-    //     i["a"] = 99;
-    //   }
-    //   return i;
-    // });
-    // console.log(this.test);
   }
 
   render() {
     let pl = { 'padding-left': '0px' };
     return (
       <div class="container">
-      {this.test.map(t => 
-        <p>{t.a}</p>
-      )}
-      <button onClick={this.changeFirstOnly.bind(this)}>Change the first position only</button>
         <form class="form-horizontal col-xs-11">
           <div class="form-group">
-            <label class="control-label col-sm-2">Related Parties </label>{/*  <!-- for inputname --> */}
+            <label class="control-label col-sm-2">{this.label}</label>{/*  <!-- for inputname --> */}
             <div class="col-xs-10" style={pl}>
               {/* Repeat the fieldset */}
               {this.data.map((e, idx) =>
@@ -97,14 +89,14 @@ export class IwMultipleValues {
                     {this.model.map((i) =>
                       <div>
                         <div class="inputinline col-xs-2">
-                          <input class="form-control input-sm" name={i.name} value={e[i.name]} type={i.type} placeholder={i.placeholder} onInput={(ev) => this.changeValue(ev,idx,i.name)} />
+                          <input class="form-control input-sm" name={i.name} value={e[i.name]} type={i.type} placeholder={i.placeholder} onInput={(ev) => this.changeValue(ev, idx, i.name)} />
                         </div>
                       </div>
                     )}
                     {/* Buttons */}
                     <div class="inputinline inputinlinebtn col-xs-2">
-                      <button type="button" ng-if="$last" class="btn btn-xs btn-primary btn-circle" onClick={this.addItem.bind(this)}><i class="fa fa-plus"></i></button>
-                      <button type="button" class="btn btn-xs btn-primary btn-circle"><i class="fa fa-minus"></i></button>
+                      {this.data.length == idx+1 ? <button type="button" ng-if="$last" class="btn btn-xs btn-primary btn-circle" onClick={this.addItem.bind(this)}><i class="fa fa-plus"></i></button> : null}
+                      {this.data.length !== 1 ? <button type="button" class="btn btn-xs btn-primary btn-circle" onClick={_ => this.removeItem(idx)}><i class="fa fa-minus"></i></button> : null}
                     </div>
                   </div>
                 </fieldset>
